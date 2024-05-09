ff87a758e410a674de083ad558f6a5fc44a50831
Marcelino Manguele <marcelinovitorinomanguele00@gmail.com>
2024-05-09 06:17:41 +0200

    imagem

--- SistemaGestaoGuestHouse/src/View/Inicio.java
+++ SistemaGestaoGuestHouse/src/View/Inicio.java
@@ -1,6 +1,21 @@
 package View;
 
-public class Inicio {
+import javax.swing.*;
+import javax.swing.plaf.ColorUIResource;
+
+public class Inicio extends JFrame {
+    public Inicio(){
+        setSize(1100,750);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+
+        //painel que ira prencher a frame
+        JPanel painel = new JPanel(null);
+        add(painel).setBounds(0,0,1100,750);
+        painel.setBackground();
+    }
+
     public static void main(String[] args) {
 
     }
